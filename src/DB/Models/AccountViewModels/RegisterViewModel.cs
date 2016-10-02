1907c3973f43875dbd36b598ee366975c6af3127
Yury Belousov <yury-belousov@yandex.ru>
2016-10-03 01:31:45 +0300

    add parameters in registerviewmodel

--- src/DB/Models/AccountViewModels/RegisterViewModel.cs
+++ src/DB/Models/AccountViewModels/RegisterViewModel.cs
@@ -6,23 +6,43 @@ using System.Threading.Tasks;
 
 namespace DB.Models.AccountViewModels
 {
-    public class RegisterViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+  public class RegisterViewModel
+  {
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
+    [Required]
+    [Display(Name = "Name")]
+    public string Name { get; set; }
 
-        
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
+    [Required]
+    [Display(Name = "Surname")]
+    public string Surname { get; set; }
+
+    [Display(Name = "Middlename")]
+    public string MiddleName { get; set; }
+
+    [Display(Name = "Phone number")]
+    [DataType(DataType.PhoneNumber)]
+    [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")
+    ]
+    public string PhoneNumber { get; set; }
+
+    [Display(Name = "Age")]
+    public int Age { get; set; }
+
+    [Required]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
   }
 }
